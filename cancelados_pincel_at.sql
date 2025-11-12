b4a4a72dbe1ced199c762b1ff1651a805cd2afae
Italo Marques <156384611+ItaloHenriqueMarquesCampos@users.noreply.github.com>
2025-11-12 14:02:52 -0300

    PINCEL ATÔMICO

--- cancelados_pincel_at.sql
+++ cancelados_pincel_at.sql
@@ -8,14 +8,15 @@ SELECT
     a.celular,
     a.telefone
 
-FROM cancelados AS c
+FROM
+    cancelados AS c
 LEFT JOIN
     curso_aluno ca ON c.matricula = ca.matricula
 LEFT JOIN
     alunos a ON ca.matricula = a.codigo
+
 WHERE
-    data >= 2025-01-01
-AND
-    ca.nivel = 'Graduação'
+    c.data BETWEEN '2025-01-01' AND '2025-10-01'
+
 ORDER BY
     c.data DESC
